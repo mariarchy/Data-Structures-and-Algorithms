267f97332950bbefeb4019e31be880e8ca90fe22
Marissa Liu <marissajliu@gmail.com>
2019-07-06 12:58:12 -0400

    Added variation of LC 122

--- Java/Array_Strings/P122-Best-Time-To-Buy-And-Sell-Stock-II.java
+++ Java/Array_Strings/P122-Best-Time-To-Buy-And-Sell-Stock-II.java
@@ -16,3 +16,28 @@ class Solution {
         return profit;
     }
 }
+
+// Variation of Problem 122 that minimizes the number of transactions you make.
+// A transaction is made whenever the profit is updated.
+class Solution2 {
+    public int maxProfit2(int[] prices) {
+        if (prices.length < 2) return 0;
+        int profit = 0;
+        int rel_min = prices[0];
+        int rel_max = rel_min;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) rel_max = prices[i];
+            else if (prices[i] < prices[i-1]) {
+              if (rel_max != rel_min) {
+                profit += rel_max - rel_min;
+              }
+              rel_min = prices[i];
+              rel_max = rel_min;
+            }
+        }
+        if (rel_max != rel_min) {
+          return profit + rel_max - rel_min;
+        }
+        else return profit;
+    }
+}
